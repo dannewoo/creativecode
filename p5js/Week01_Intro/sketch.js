1737b68dc317d539968874f582180b70b1aeb924
Danne Woo <danne@dannewoo.com>
2017-02-11 11:30:17 -0500

    Added Week 1 p5js example.

---
+++ p5js/Week01_Intro/sketch.js
@@ -0,0 +1,13 @@
+//Setup runs through your code once.
+//The computer reads your code from top to bottom.
+function setup() {
+  //Dimensions of your canvas.
+  createCanvas(400, 300);
+  //Background color of your canvas .
+  //Color ranges from 0 (black) to 255 (white).
+  background(200);
+  fill(50);
+  rect(0, 0, 100, 100);
+  //Printing out information to the javascript console below.
+  print("Hello world!!!");
+}
